de996bfff30d1a2333ac613e8f2b72a987d1f099
Jason Smylnycky <jason784@gmail.com>
2016-03-25 22:03:36 -0400

    Initial setup with query to guild

---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let express = require('express');
+let helmet = require('helmet');
+let router = express.Router();
+let session = require('express-session');
+let app = express();
+
+// Security
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(session({
+   secret: 's3Cur3',
+   name: 'sessionId',
+  })
+);
+
+app.use('/v1', router)
+app.set('port', (process.env.PORT || 5000));
+
+require('./lib/guild/index')(router);
+
+
+app.listen(app.get('port'), () => {
+  console.log('No Breaks API is running on port', app.get('port'));
+});
