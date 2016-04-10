645ac2eb9dbd6b9ce255edf9d0ed2f4f581ed07c
Jason Smylnycky <jason784@gmail.com>
2016-04-10 08:54:50 -0400

    Squashed commit of the following:
    commit 392719abd4fa7f1bc59ca3f95216c2f9fa453056
    Author: Jason Smylnycky <jason784@gmail.com>
    Date:   Fri Apr 8 19:20:54 2016 -0400
    
        more cleanup
    
    commit f6594028870f04b8fb12a5919a3540b5c0ecf939
    Author: Jason Smylnycky <jason784@gmail.com>
    Date:   Fri Apr 8 19:20:26 2016 -0400
    
        Cleanup
    
    commit 759a150ac510bf725199590c15d19e29bf8d88c6
    Author: Jason Smylnycky <jason784@gmail.com>
    Date:   Fri Apr 8 18:59:06 2016 -0400
    
        Initial service that gets roster

---
+++ services/update-roster.js
@@ -0,0 +1,80 @@
+'use strict';
+
+let app = require('../index');
+let Character = require('../lib/character/model');
+let helpers = require('../lib/helpers');
+let fetch = require('node-fetch');
+let Promise = require('es6-promise').Promise;
+let _ = require('lodash');
+
+const GUILD_API_PATH = '/wow/guild/Thrall/No%20Breaks';
+
+function getMembers() {
+  let query = {
+    fields: 'members',
+    locale: 'en_US'
+  };
+
+  let raiders = [];
+  let members = [];
+
+  fetch(helpers.buildGetUrl(GUILD_API_PATH, query))
+  	.then(helpers.parseFetchResponse)
+    .then((json) => {
+
+      json.members = json.members.filter(function (el) {
+        return el.rank == 0 || el.rank == 1 || el.rank == 4 || el.rank == 5 ;
+      });
+
+      _.forEach(json.members, (member) => {
+
+        let promises = [];
+
+        let promise = new Promise((resolve, reject) => {
+          fetch('http://127.0.0.1:5000/v1/character/' + member.character.realm + '/' + encodeURIComponent(member.character.name) + '/items')
+            .then(helpers.parseFetchResponse)
+            .then((json) => {
+              member.items = {
+                averageItemLevel: json.items.averageItemLevel,
+                averageItemLevelEquipped: json.items.averageItemLevelEquipped
+              };
+              resolve(member);
+            }, (error) => {
+              reject(error);
+            });
+        });
+
+        promises.push(promise);
+
+        _.forEach(promises, (promise) => {
+
+          promise.then((member) => {
+            let memberObj = {
+              name: member.character.name,
+              class: member.character.class,
+              race: member.character.race,
+              gender: member.character.gender,
+              level: member.character.level,
+              achievementPoints: member.character.achievementPoints,
+              thumbnail: member.character.thumbnail,
+              calcClass: member.character.calcClass,
+              rank: member.rank
+            };
+
+            Character.findOneAndUpdate({name: member.name}, member, {upsert: true}, (err, doc) => {
+              if (err) { throw new Error(err); }
+            });
+          }, (error) => {
+            console.log(error);
+          });
+
+          helpers.sleep(500);
+        });
+      });
+    }, (error) => {
+      reject(error);
+    });
+}
+
+
+getMembers();
