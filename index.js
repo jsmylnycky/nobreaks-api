74418b3f2baaa96adf2d84032d5f3521d70ca119
Jason Smylnycky <jason784@gmail.com>
2016-08-27 19:47:04 -0400

    v1.4.0 oauth Squashed commit of the following:
    commit 3933442cc130b13be43c43128440e6fca29feb5b
    Author: Jason Smylnycky <jason784@gmail.com>
    Date:   Sat Aug 27 19:41:08 2016 -0400
    
        Completed auth

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+let bodyParser = require('body-parser');
 let cors = require('cors');
 let express = require('express');
 let helmet = require('helmet');
 let mongoose = require('mongoose');
+let passportConfig = require('./lib/auth/passport');
 let router = express.Router();
-let session = require('express-session');
-let bodyParser = require('body-parser');
-var serveStatic = require('serve-static')
+let serveStatic = require('serve-static');
+let conf = require('./nobreaks.conf.json');
+// let session = require('express-session');
+// let MongoStore = require('connect-mongo')(session);
 
 let app = module.exports = express();
 
@@ -20,11 +23,21 @@ app.set('trust proxy', 1) // trust first proxy
 
 app.use(helmet());
 app.disable('x-powered-by');
-app.use(session({
-   secret: 's3Cur3',
-   name: 'sessionId',
+
+// Database setup
+mongoose.connect('mongodb://127.0.0.1:27017/nobreaks')
+mongoose.Promise = global.Promise;
+
+app.set('superSecret', conf.jwt.secret);
+
+/* app.use(session({
+    secret: 'n0bre4k$!',
+    cookie: { secure: true },
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
   })
-);
+); */
 
 // Form submission
 app.use(bodyParser.json()); // support json encoded bodies
@@ -32,10 +45,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // Routes setup
 app.use('/v1', router)
-
-// Database setup
-mongoose.connect('mongodb://127.0.0.1:27017/nobreaks')
-mongoose.Promise = global.Promise;
 
 // production error handler
 // no stacktraces leaked to user
@@ -48,11 +57,31 @@ app.use(serveStatic(__dirname + '/static', {
   'index': false
 }));
 
+// Passport
+passportConfig(app);
+
+require('./lib/auth/index')(app);
 require('./lib/character/index')(router);
 require('./lib/epgp/index')(router);
 require('./lib/guild/index')(router);
 require('./lib/item/index')(router);
 
-app.listen(app.get('port'), () => {
-  console.log('No Breaks API is running on port', app.get('port'));
-});
+if (app.get('env') === 'development') {
+  let fs = require('fs');
+  let https = require('https');
+
+  let sslOptions = {
+    key: fs.readFileSync(__dirname + '/ssl/localhost.key'),
+    cert: fs.readFileSync(__dirname + '/ssl/localhost.crt')
+  };
+
+  var server = https.createServer(sslOptions, app).listen(app.get('port'), function() {
+    console.log('Listening on port %d', app.get('port'));
+    console.log('To test this example, navigate to https://localhost:' + app.get('port') +'/auth/bnet');
+    console.log('You\'ll have to accept a security exception for the self signed SSL certificate.');
+  });
+} else {
+  app.listen(app.get('port'), () => {
+    console.log('No Breaks API is running on port', app.get('port'));
+  });
+}
